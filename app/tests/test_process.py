292bcaebad5a4bb7a84f0a4c15c1e1fee5fbd8dc
David Amores <davidamores@gmail.com>
2024-01-20 07:25:48 +1100

    Pasar chequeo de formato

--- app/tests/test_process.py
+++ app/tests/test_process.py
@@ -1,8 +1,10 @@
+"""Pruebas para el modulo process.py."""
 from unittest.mock import patch
 
 import pytest
 
 from app.process import num_characters, average_word_size, text_analysis
+
 
 @pytest.mark.parametrize(
     "text,expected",
@@ -15,19 +17,26 @@ from app.process import num_characters, average_word_size, text_analysis
     ]
 )
 def test_num_characters(text, expected):
+    """Prueba para la funcion num_characters."""
     with patch("app.process.almacenar_en_db") as mock_db:
         assert num_characters(text) == expected
         mock_db.assert_called_once_with(expected)
 
+
 def test_average_word_size():
+    """Prueba para la funcion average_word_size."""
     assert average_word_size("texto ejemplo") == 6.0
 
+
 def test_average_word_size_empty():
+    """Prueba para la funcion average_word_size con texto vacio."""
     with pytest.raises(ZeroDivisionError):
         average_word_size("")
 
+
 def test_text_analysis():
-    with patch("app.process.almacenar_en_db") as mock_db:
+    """Prueba para la funcion text_analysis."""
+    with patch("app.process.almacenar_en_db"):
         assert text_analysis("texto ejemplo") == (
             "Texto: texto ejemplo"
             "<br>"
